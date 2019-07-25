7a2533ee38f664adbe80bf4d5b54c9b7f1bf4dfd
Gabriel A <33150863+Joimo@users.noreply.github.com>
2019-07-25 16:48:46 -0300

    git push

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ $('.get-device').on('click',function(){
     navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
     .then(device => {
         console.log(device);
-        $('.device-name').val(device.name)        
+        $('.device-name').val(device.name)
     })
+    .then(service => {
+      // Getting Battery Level Characteristic...
+      return service.getCharacteristic('battery_level');
+    })
+    .then(characteristic => {
+      // Reading Battery Level...
+      return characteristic.readValue();
+    })
+    .then(value => {
+      console.log('Battery percentage is ' + value.getUint8(0));
+    })
+    
     .catch(error => { console.log(error); });
 });
