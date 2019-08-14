107ff2ed8be7b18259c375f4291f4f6503d39c85
Gabriel A <33150863+Joimo@users.noreply.github.com>
2019-08-13 22:40:17 -0300

    git push

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
-console.log('inicio');
+console.log('Iniciando..');
 
-$('.get-device').on('click',function(){
-    navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
-    .then(device => {
-        console.log(device);
-        $('.device-name').val(device.name);
-        
-    })
-    .then(device => {
-      log('Connecting to GATT Server...');
-      return device.gatt.connect();
-    })
-    .then(server => {
-      log('Getting Battery Service...');
-      return server.getPrimaryService('battery_service');
-    })
-    .then(service => {
-      log('Getting Battery Level Characteristic...');
-      return service.getCharacteristic('battery_level');
-    })
-    .then(characteristic => {
-      log('Reading Battery Level...');
-      return characteristic.readValue();
-    })
-    .then(value => {
-      let batteryLevel = value.getUint8(0);
-      log('> Battery Level is ' + batteryLevel + '%');
-    })
-    .catch(error => {
-      log('Argh! ' + error);
-    });
-});
+function onButtonClick() {
+  log('Requesting Bluetooth Device...');
+  navigator.bluetooth.requestDevice(
+    {filters: [{services: ['battery_service']}]})
+  .then(device => {
+    log('Connecting to GATT Server...');
+    return device.gatt.connect();
+  })
+  .then(server => {
+    log('Getting Battery Service...');
+    return server.getPrimaryService('battery_service');
+  })
+  .then(service => {
+    log('Getting Battery Level Characteristic...');
+    return service.getCharacteristic('battery_level');
+  })
+  .then(characteristic => {
+    log('Reading Battery Level...');
+    return characteristic.readValue();
+  })
+  .then(value => {
+    let batteryLevel = value.getUint8(0);
+    log('> Battery Level is ' + batteryLevel + '%');
+  })
+  .catch(error => {
+    log('Argh! ' + error);
+  });
+}
