221c88d990ac2c0d03dc471b264dcce728214854
Gabriel A <33150863+Joimo@users.noreply.github.com>
2019-08-13 21:57:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,30 +5,7 @@ $('.get-device').on('click',function(){
     .then(device => {
         console.log(device);
         $('.device-name').val(device.name);
+        $('.device-level').val(device.id);
     })
-    .then(server => {
-      // Getting Battery Service...
-      return server.getPrimaryService('battery_service');
-    })
-    .then(service => {
-      // Getting Battery Level Characteristic...
-      return service.getCharacteristic('battery_level');
-    })
-    .then(characteristic => {
-      // Reading Battery Level...
-      return characteristic.readValue();
-    })
-    .then(value => {
-      console.log('Battery percentage is ' + value.getUint8(0));
-      $('.device-level').val(device.value)
-
-    })
-    
-
-    .catch(error => { console.log(error); });
-
-    function handleBatteryLevelChanged(event) {
-      let batteryLevel = event.target.value.getUint8(0);
-      console.log('Battery percentage is ' + batteryLevel);
-    }
+        
 });
