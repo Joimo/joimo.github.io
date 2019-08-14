c58e51f6101420e99120f6a37d0a60382781d426
Gabriel A <33150863+Joimo@users.noreply.github.com>
2019-08-13 22:25:17 -0300

    git push

--- main.js
+++ main.js
@@ -5,7 +5,18 @@ $('.get-device').on('click',function(){
     .then(device => {
         console.log(device);
         $('.device-name').val(device.name);
-        $('.device-level').val(device.id);
+        $('.device-level').val(device.name);        
     })
+  .then(device => device.connectGATT())
+  .then(server => {
+    return server.getPrimaryService('battery_service');
+  })
+  .then(service => {
+    return service.getCharacteristic('battery_level');
+  })
+  .then(characteristic => {
+    // Read battery level...
+    return characteristic.readValue();
+  });
         
 });
