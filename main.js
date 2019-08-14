52bb3f9222e09d2b7c5ffc3b1e4514193d244c93
Gabriel A <33150863+Joimo@users.noreply.github.com>
2019-08-13 21:51:46 -0300

    git push

--- main.js
+++ main.js
@@ -4,23 +4,26 @@ $('.get-device').on('click',function(){
     navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
     .then(device => {
         console.log(device);
-        $('.device-name').val(device.name)
-        .then(service => {
-          console.log('Getting Battery Level Characteristic...');
-          return service.getCharacteristic('battery_level');
-        })
-      })
-    
+        $('.device-name').val(device.name);
+    })
+    .then(server => {
+      // Getting Battery Service...
+      return server.getPrimaryService('battery_service');
+    })
+    .then(service => {
+      // Getting Battery Level Characteristic...
+      return service.getCharacteristic('battery_level');
+    })
     .then(characteristic => {
-      // Set up event listener for when characteristic value changes.
-      characteristic.addEventListener('characteristicvaluechanged', handleBatteryLevelChanged());
-
       // Reading Battery Level...
       return characteristic.readValue();
     })
     .then(value => {
       console.log('Battery percentage is ' + value.getUint8(0));
+      $('.device-level').val(device.value)
+
     })
+    
 
     .catch(error => { console.log(error); });
 
