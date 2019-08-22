668c7c6d10d63084044c70c29cd2c60b369ea5e2
Gabriel A <33150863+Joimo@users.noreply.github.com>
2019-08-22 14:36:26 -0300

    git push

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
+let myDevice;
+
 function connect() {
   const device = navigator.bluetooth.requestDevice({
     filters:
     [
-      { namePrefix: 'BLE'}
-    ]
-  });
-  console.log(device.name);
-  $('.device-name').val(device.name)
+      { namePrefix: 'BLE'},
+    ]   
+  })
+  alert(device.namePrefix)
+
+  .then(device => { 
+    myDevice = device;
+    return device.gatt.connect();
+  })
+  
+  .then(server => getPrimaryService('0000ffe0-0000-1000-8000-00805f9b34fb'))
+  alert('yes!')
+  .catch(error => { console.log(error); });  
+
 }
+
