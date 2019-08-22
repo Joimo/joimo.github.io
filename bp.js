fb188e43ce7b3af00ce5b86041379e20550d3318
Gabriel A <33150863+Joimo@users.noreply.github.com>
2019-08-22 10:36:23 -0300

    git push

--- bp.js
+++ bp.js
@@ -48,3 +48,41 @@ async function bat(){
     bat();
   },5000);            
 };
+
+
+
+if (!navigator.bluetooth) {
+  alert('Desculpe, seu navegador não suporta Bluetooth API');
+}
+
+const SEND_SERVICE = 0xFFE0;
+const ligaLedBtn = document.getElementById('ligaLed');
+
+let LedCharacteristic;
+
+async function btn() {
+  try {
+    console.log('Requesting Bluetooth Device...');
+    const device = await navigator.bluetooth.requestDevice({
+      filters: [
+        { name: 'BLE' },
+        { namePrefix: 'B'},
+        { services: [SEND_SERVICE] },
+      ]      
+    });    
+    $('.device-name').val(device.name)
+ 
+    
+    console.log('Connecting to GATT Server...');    
+    const server = await device.gatt.connect();
+
+    console.log('Getting Primary Service...');
+    const service = await server.getPrimaryService('SEND_SERVICE');
+
+    console.log('Getting Characteristic...');
+    const characteristic = await service.getCharacteristic('SEND_SERVICE');          
+            
+  } catch (error) {
+    console.log('Errooo! ' + error);
+  }
+}
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 if (!navigator.bluetooth) {
-  alert('Desculpe, seu navegador não suporta Bluetooth API '<br>'Utilize a versão mais atualizada do Google Chrome' );
+  alert('Desculpe, seu navegador não suporta Bluetooth API');
 }
-
 
 const SEND_SERVICE = 0xFFE0;
 const ligaLedBtn = document.getElementById('ligaLed');
+
+let LedCharacteristic;
 
 async function btn() {
   try {
@@ -14,22 +15,55 @@ async function btn() {
         { name: 'BLE' },
         { namePrefix: 'B'},
         { services: [SEND_SERVICE] },
-      ]      
-    });    
+      ]        
+      .then(device => {
+        myDevice = device;
+        return device.gatt.connect();
+      })         
+      .then(server => server.getPrimaryService(SEND_SERVICE))
+      .then(service => service.getCharacteristic(SEND_SERVICE_CHARACTERISTIC))  
+      .then(characteristic => {
+        LedCharacteristic = characteristic;   
 
-    $('.device-name').val(device.name)
- 
-    console.log('Connecting to GATT Server...');    
-    const server = await device.gatt.connect();
 
-    console.log('Getting Primary Service...');
-    const service = await server.getPrimaryService('SEND_SERVICE');
+        btnClick('addEventListener');
+      })
+      .catch(error => {
+        console.log('Errooo! ' + error);     
+      })
+    });
 
-    console.log('Getting Characteristic...');
-    const characteristic = await service.getCharacteristic('SEND_SERVICE');
-          
+    //$('.device-name').val(device.name)
+
+    
+    //console.log('Connecting to GATT Server...');    
+    //const server = await device.gatt.connect();
+
+    //console.log('Getting Primary Service...');
+    //const service = await server.getPrimaryService('SEND_SERVICE');
+
+    //console.log('Getting Characteristic...');
+    //const characteristic = await service.getCharacteristic('SEND_SERVICE');          
+    function acendeLED(event) {
+      const code = Number(event.target.dataset.code);
+
+      if(code = 1) {
+        LedCharacteristic.writeValue(Uint8Array.of(code));
+        return;
+      }
+
+      LedCharacteristic.readValue()
+        .then(currentCode =>  {
+          const converteCode = currentCode.getUint8(0);
+
+          LedCharacteristic.writeValue(Uint8Array.of(converteCode === code ? 0 : code));
+        });
+    }
+    function btnClick(action) {
+      ligaLedBtn[action]('click',acendeLED);
+    }
             
   } catch (error) {
-    console.log('Errooo! ' + error);
+    console.log('Errooo! - 2 ' + error)
   }
 }
