080f3061621a6dfd16d795b645c69229f1b06adb
Gabriel A <33150863+Joimo@users.noreply.github.com>
2019-09-02 09:52:02 -0300

    git push

--- blockly/workspace.js
+++ blockly/workspace.js
@@ -18,3 +18,40 @@ Blockly.JavaScript['liga_led'] = function(block) {
 	console.log(valor);
 	return valor;
   };
+
+
+if (!navigator.bluetooth) {
+	alert('Sorry, your browser doesn\'t support Bluetooth API');
+}
+
+const connectButton = document.getElementById('connectButton');
+const toggleLigthCharacteristic;
+
+connectButton.addEventListener('pointerup', connectButtonPointerUpHandler);
+
+function connectButtonPointerUpHandler() {
+  navigator.bluetooth.requestDevice({
+    filters:
+      [
+        { name: 'BLE' },
+        { namePrefix: 'B'},
+        { services: [SEND_SERVICE] },
+      ]
+  })
+    .then(device => {
+      myDevice = device;
+
+      return device.gatt.connect();
+    })
+    .then(server => server.getPrimaryService(SEND_SERVICE))
+    .then(service => service.getCharacteristic(SEND_SERVICE_CHARACTERISTIC))
+    .then(characteristic => {
+      toggleLigthCharacteristic = characteristic;
+
+      toggleButtonsVisible();
+      toggleItemsEventListeners('addEventListener');
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
