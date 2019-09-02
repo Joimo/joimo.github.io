aa8f05c23db21c71d535e96fd7121235d3f56aa7
Gabriel A <33150863+Joimo@users.noreply.github.com>
2019-09-02 09:39:12 -0300

    git push

---
+++ blockly/drop.js
@@ -0,0 +1,7 @@
+Blockly.JavaScript['liga_led'] = function(block) {
+  var dropdown_acao = block.getFieldValue('acao');
+  var dropdown_acao2 = block.getFieldValue('acao2');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
---
+++ blockly/workspace.js
@@ -0,0 +1,20 @@
+Blockly.Blocks['liga_led'] = {
+	init: function() {
+	  this.appendValueInput("7")
+		  .setCheck(null)
+		  .appendField("Ligar Led");
+	  this.setColour(120);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
+
+Blockly.JavaScript['liga_led'] = function(block) {
+	console.log("cheguei");
+	var value_7 = Blockly.JavaScript.valueToCode(block, '7', Blockly.JavaScript.ORDER_ATOMIC);	
+	var valor = 7;
+	//var code = "";
+	//console.log(code);	
+	console.log(valor);
+	return valor;
+  };
